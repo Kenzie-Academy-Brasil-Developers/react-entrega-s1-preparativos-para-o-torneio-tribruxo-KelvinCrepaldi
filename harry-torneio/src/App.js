a56e0f91582c6f22019a3c0193d61a3ee9f4c2cc
Kelvin Crepaldi <kelvin.creps@hotmail.com>
2021-09-13 15:34:29 -0300

    correcao no link da API

--- harry-torneio/src/App.js
+++ harry-torneio/src/App.js
@@ -10,7 +10,7 @@ function App() {
   const [selectedStudents, setSelectedStudents] = useState([]);
 
   useEffect(() => {
-    fetch(`http://hp-api.herokuapp.com/api/characters/students`)
+    fetch(`https://hp-api.herokuapp.com/api/characters/students`)
       .then((response) => response.json())
       .then((response) => setStudents(response))
       .catch((err) => console.log(err));
